840418c09809f2faefc1dcdaecf9487035691f70
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-01-18 03:50:28 +0000

    Item12355: add JQuery DataTables plugin for progressive enhancement of tables
    git-svn-id: http://svn.foswiki.org/trunk/JQDataTablesPlugin@16403 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Plugins/JQDataTablesPlugin.pm
@@ -0,0 +1,31 @@
+# See end of file for license and copyright information
+package Foswiki::Plugins::JQDataTablesPlugin;
+
+use strict;
+use Assert;
+
+use Foswiki::Func    ();    # The plugins API
+use Foswiki::Plugins ();    # For the API version
+
+our $VERSION           = '$Rev: 5771 $';
+our $RELEASE           = '1.1.2';
+our $SHORTDESCRIPTION  = 'Jquery based progressive enhancement of tables';
+our $NO_PREFS_IN_TOPIC = 1;
+
+sub initPlugin {
+    my ( $topic, $web, $user, $installWeb ) = @_;
+
+    return 1;
+}
+
+1;
+__END__
+Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+JQTablePlugin is copyright (C)SvenDOwideit@fosiki.com
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+For licensing info read LICENSE file in the root of this distribution.
---
+++ lib/Foswiki/Plugins/JQDataTablesPlugin/Config.spec
@@ -0,0 +1,11 @@
+# ---+ Extensions
+# ---++ JQDataTablesPlugin
+
+
+# ---+ Extensions
+# ---++ JQueryPlugin
+# ---+++ Extra plugins
+# **STRING**
+$Foswiki::cfg{JQueryPlugin}{Plugins}{datatables}{Module} = 'Foswiki::Plugins::JQDataTablesPlugin::DataTables';
+# **BOOLEAN**
+$Foswiki::cfg{JQueryPlugin}{Plugins}{datatables}{Enabled} = 1;
